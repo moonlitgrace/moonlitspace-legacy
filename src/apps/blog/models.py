e6e32e7ed3a1b35dc5e7d54879e3835c27e4d257
tokitouq <suneeth.since2005@gmail.com>
2024-07-08 16:51:35 +0530

    fix: poetry config issue

--- src/apps/blog/models.py
+++ src/apps/blog/models.py
@@ -3,7 +3,6 @@ from django.db import models
 # from django_prose_editor.fields import ProseEditorField
 # from django_markdown.models import MarkdownField
 
-
 class BlogPost(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField(editable=False)
