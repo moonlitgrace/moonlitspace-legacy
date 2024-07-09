8989a36f14078b4eed9a8632dcd45bb7310d8b82
tokitouq <suneeth.since2005@gmail.com>
2024-07-10 01:01:57 +0530

    feat: used base error template for exception cases

--- apps/newsletter/urls.py
+++ apps/newsletter/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.newsletter_validate_view, name="newsletter_validate"),
+    path("verify-email/<int:entry_id>/", views.newsletter_verify_view, name="newsletter_verify"),
 ]
--- apps/newsletter/views.py
+++ apps/newsletter/views.py
@@ -6,5 +6,15 @@ from .models import NewsLetterEntry
 def newsletter_validate_view(request):
     email = request.POST.get("email")
     if NewsLetterEntry.objects.filter(email=email).exists():
-        return render(request, "newsletter/validation_failed.html")
+        return render(request, "newsletter/error.html", context={
+            "error_message": "Email already subscribed, please try another one.",
+        })
     return render(request, "newsletter/verification_email_send.html", {"email": email})
+
+def newsletter_verify_view(request, entry_id):
+    try:
+        entry = NewsLetterEntry.objects.get(entry_id=entry_id)
+        entry.verified = True
+        entry.save()
+    except NewsLetterEntry.DoesNotExist:
+        pass
