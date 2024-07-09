00abac1d9bcb9f2de54aaf6a12f282d0bc4bd906
tokitouq <suneeth.since2005@gmail.com>
2024-07-09 13:08:18 +0530

    fix: markdown_to_text filter

--- utils/markdown_to_text.py
+++ utils/markdown_to_text.py
@@ -1,4 +1,5 @@
 import markdown2
+import re
 from selectolax.parser import HTMLParser
 
 def markdown_to_text(md_string):
@@ -8,4 +9,6 @@ def markdown_to_text(md_string):
     # get only paragraphs tags text
     p_tags = tree.css("p")
     text = "\n".join([p.text() for p in p_tags])
+    # remove code snippets
+    text = re.sub(r'```.*?```', '', text, flags=re.DOTALL)
     return text
