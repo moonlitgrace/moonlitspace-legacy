5aab09790e46cf5f3ef48ee0706e8e9b0753f61c
tokitouq <suneeth.since2005@gmail.com>
2024-07-09 13:10:05 +0530

    feat: add markdown tag to pinned posts

--- core/views.py
+++ core/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
 
-from utils.markdown_to_text import markdown_to_text
-
 from apps.user.models import Profile
 from apps.blog.models import PinnedPost, BlogPost
 
@@ -9,9 +7,6 @@ def index(request):
     profile = Profile.objects.get(type="anonymous", active=True)
     pinned_posts = PinnedPost.objects.all()
     latest_posts = BlogPost.objects.all().exclude(pk__in=pinned_posts)
-
-    for post in pinned_posts:
-        post.content = markdown_to_text(post.post.content)
 
     context = {
         "profile": profile,
