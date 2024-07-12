30885c926c771a478f2dfa6c6f25c2ca19dce2e6
tokitouq <suneeth.since2005@gmail.com>
2024-07-12 09:06:42 +0530

    refactor: add instance handling for recipient_list

--- utils/mail.py
+++ utils/mail.py
@@ -2,6 +2,7 @@ from django.core import mail
 from django.template.loader import render_to_string
 from typing import Union, Optional
 
+
 def send_moonlit_mail(
     subject: str,
     recipient_list: Union[list[str], str],
@@ -9,8 +10,8 @@ def send_moonlit_mail(
     context: Optional[dict] = None,
     error_template_name: Optional[str] = None,
 ):
-    """ check if recipient list is a list
-    or not - conver to a list """
+    """check if recipient list is a list
+    or not - conver to a list"""
     if not isinstance(recipient_list, list):
         recipient_list = list(recipient_list)
 
