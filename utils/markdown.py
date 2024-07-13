ecf86553c0f7c0c4004a530ced943deb1854341a
tokitouq <suneeth.since2005@gmail.com>
2024-07-13 17:15:39 +0530

    feat: add pygments and custom renderer with highlights

--- utils/markdown.py
+++ utils/markdown.py
@@ -1,3 +1,15 @@
 import mistune
+from pygments import highlight
+from pygments.lexers import get_lexer_by_name
+from pygments.formatters import html
 
-markdown = mistune.Markdown(renderer=mistune.HTMLRenderer())
+
+class CustomRenderer(mistune.HTMLRenderer):
+    def block_code(self, code, info=None):
+        if info:
+            lexer = get_lexer_by_name(info, stripall=True)
+            formatter = html.HtmlFormatter(cssclass="codehilite")
+            return highlight(code, lexer, formatter)
+        return "<pre><code>" + mistune.escape(code) + "</code></pre>"
+
+markdown = mistune.Markdown(renderer=CustomRenderer())
