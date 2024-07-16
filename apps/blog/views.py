50aa94c447844a1becef73b3c0b1557e42f77c76
tokitouq <suneeth.since2005@gmail.com>
2024-07-16 10:29:38 +0530

    refactor: get first object from filter
    Prevents obj.DoesNotExists error with filter method

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -8,7 +8,7 @@ from .models import BlogPost, PinnedPost
 
 # Create your views here.
 def index(request):
-    profile = Profile.objects.get(type="anonymous", active=True)
+    profile = Profile.objects.filter(type="anonymous", active=True).first()
     pinned_posts = PinnedPost.objects.all()
     latest_posts = BlogPost.objects.all().exclude(pk__in=pinned_posts)
 
