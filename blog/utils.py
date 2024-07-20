2ab5ee64229f76fb7f2b58392b089415759adbd4
tokitouq <suneeth.since2005@gmail.com>
2024-07-20 13:23:34 +0530

    chore: ad catppuccin theme for pygments

--- blog/utils.py
+++ blog/utils.py
@@ -4,6 +4,7 @@ import mistune
 from pygments import highlight
 from pygments.lexers import get_lexer_by_name
 from pygments.formatters import html
+from pygments.styles import get_style_by_name
 
 # plugins
 from mistune.plugins.formatting import strikethrough
@@ -17,7 +18,7 @@ class CustomRenderer(mistune.HTMLRenderer):
     def block_code(self, code, info=None):
         if info:
             lexer = get_lexer_by_name(info, stripall=True)
-            formatter = html.HtmlFormatter(cssclass="codehilite")
+            formatter = html.HtmlFormatter(cssclass="codehilite", style=get_style_by_name("catppuccin-mocha"))
             return highlight(code, lexer, formatter)
         return "<pre><code>" + mistune.escape(code) + "</code></pre>"
 
