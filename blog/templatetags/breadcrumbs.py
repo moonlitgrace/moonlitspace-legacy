58c874f8572098358aee6a7f8dc7a92a3eab7dec
tokitouq <suneeth.since2005@gmail.com>
2024-07-22 22:31:44 +0530

    chore: remove space reference from blog app template

--- blog/templatetags/breadcrumbs.py
+++ blog/templatetags/breadcrumbs.py
@@ -8,7 +8,7 @@ def breadcrumb_path(path: str):
     # eg path: /blog/all/
     # output: [{"name": "blog", "url": "/blog"}, ...]
     segments = path.strip("/").split("/")
-    breadcrumbs = [{"name": "space", "url": "/"}]
+    breadcrumbs = []
     url = ""
 
     for segment in segments:
